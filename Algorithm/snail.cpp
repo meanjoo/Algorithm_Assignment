9333d4abb66ff09833572172f5978b7b20efdde1
meanjoo <lead1211@naver.com>
2022-09-15 16:02:44 +0900

    .

---
+++ Algorithm/snail.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+typedef long long ll;
+
+void solve() {
+	ll n, a, b;
+	cin >> n >> a >> b;
+
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	freopen("snail.inp", "r", stdin);
+	freopen("snail.out", "w", stdout);
+
+	int T;
+	cin >> T;
+	for (int tc = 1; tc <= T; tc++) {
+		solve();
+	}
+	return 0;
+}
