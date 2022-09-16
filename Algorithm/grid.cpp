338a351b96f523e0509311284fc3a31305cd07a9
meanjoo <meanjooo@naver.com>
2022-09-16 15:53:16 +0900

    06_grid

--- Algorithm/grid.cpp
+++ Algorithm/grid.cpp
@@ -2,18 +2,6 @@
 #define endl '\n'
 using namespace std;
 
-vector<int> paint;
-
-// sz: 1줄의 크기, target: 최소 개수
-bool canPaint(int sz, int target) {
-	int cnt = 0;
-	for (auto& p : paint) {
-		if ((int)(p / sz) >= 2)
-			cnt += (int)(p / sz);
-	}
-	return cnt >= target;
-}
-
 int odd(int num, int d, bool& chk) {
 	if (num / d >= 3)
 		chk = true;
@@ -27,11 +15,9 @@ int even(int num, int d) {
 void solve() {
 	int N, M, K, paint;
 	cin >> N >> M >> K;
-	//paint.resize(K);
 	int vertical = 0, horizontal = 0;
 	bool chkVertical = M % 2 == 0 ? true : false;
 	bool chkHorizontal = N % 2 == 0 ? true : false;
-	cout << "v: " << chkVertical << ", h : " << chkHorizontal << endl;
 	for (int i = 0; i < K; i++) {
 		cin >> paint;
 		if (!chkVertical)
@@ -40,13 +26,11 @@ void solve() {
 			vertical += even(paint, N);
 
 		if (!chkHorizontal)
-			horizontal += odd(paint, N, chkHorizontal);
+			horizontal += odd(paint, M, chkHorizontal);
 		else
 			horizontal += even(paint, M);
 	}
-	//cout << "vertical: " << vertical << ", horizontal: " << horizontal << endl;
-	cout << "vertical: " << vertical << ", " << chkVertical << " / horizontal: " << horizontal << ", " << chkHorizontal << endl;
-	//(vertical >= K && chkVertical) || (horizontal >= K && chkHorizontal) ? cout << "YES" << endl : cout << "NO" << endl;
+	(vertical >= M && chkVertical) || (horizontal >= N && chkHorizontal) ? cout << "Yes" << endl : cout << "No" << endl;
 }
 
 int main() {
