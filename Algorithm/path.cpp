26833b9e139311c4831ff174220016c765a45dd1
meanjoo <meanjooo@naver.com>
2022-10-07 04:23:28 +0900

    .

--- Algorithm/path.cpp
+++ Algorithm/path.cpp
@@ -1,11 +1,15 @@
 #include <bits/stdc++.h>
 #define endl '\n'
+#define x first
+#define y second
 #define MOD 100000
+#define MAX 99999999
 using namespace std;
+typedef pair<int, int> Point;
 
 typedef struct _myStruct {
 	int cnt[202], min[202];
-	string path[202];
+	Point path[202];
 }myStruct;
 
 bool mark[100][100];
@@ -18,12 +22,13 @@ void init() {
 		for (int j = 0; j < N; j++) {
 			for (int k = 0; k <= K + 1; k++) {
 				dp[i][j].cnt[k] = 0;
-				dp[i][j].min[k] = 0;
-				dp[i][j].path[k] = "";
+				dp[i][j].min[k] = MAX;
 			}
 			mark[i][j] = false;
 		}
 	}
+	for (int i = 0; i <= K + 1; i++)
+		dp[0][0].min[i] = 0;
 }
 
 void getInput() {
@@ -42,27 +47,8 @@ void getInput() {
 	}
 }
 
-void addPathRight(int row, int col, int i) {
-	dp[row][col].min[i] = dp[row][col - 1].min[i] + rowCost[row][col];
-	dp[row][col].path[i] = dp[row][col - 1].path[i] + "R";
-}
-
-void addPathDown(int row, int col, int i) {
-	dp[row][col].min[i] = dp[row - 1][col].min[i] + colCost[row][col];
-	dp[row][col].path[i] = dp[row - 1][col].path[i] + "D";
-}
-
-char direction(string& fromUp, string& fromLeft) {
-	int len = (int)fromUp.length();
-	for (int i = 0; i < len; i++) {
-		if (fromUp[i] == fromLeft[i])
-			continue;
-		return fromUp[i] == 'D' ? 'd' : 'r';
-	}
-}
-
-void arrowPoint(int x, int y) {
-	cout << "->(" << x << "," << y << ")";
+int min3(int a, int b, int c) {
+	return a < b && a < c? a : b < c ? b : c;
 }
 
 void solve() {
@@ -71,52 +57,35 @@ void solve() {
 	getInput();
 
 	// 0행
-	string str = "R";
 	for (int col = 1, chk = 0; col < N; col++) {
 		int prevCost = dp[0][col - 1].min[chk];
 		if (mark[0][col])
 			++chk;
 		dp[0][col].cnt[chk] = 1;
 		dp[0][col].min[chk] = prevCost + rowCost[0][col];
-		dp[0][col].path[chk] = str;
-		str.push_back('R');
+		dp[0][col].path[chk] = { 0,col - 1 };
 	}
 
 	// 0열
-	str = "D";
 	for (int row = 1, chk = 0; row < M; row++) {
 		int prevCost = dp[row - 1][0].min[chk];
 		if (mark[row][0])
 			++chk;
 		dp[row][0].cnt[chk] = 1;
 		dp[row][0].min[chk] = prevCost + colCost[row][0];
-		dp[row][0].path[chk] = str;
-		str.push_back('D');
+		dp[row][0].path[chk] = { row - 1,0 };
 	}
 
 	for (int row = 1; row < M; row++) {
 		for (int col = 1; col < N; col++) {
 			for (int i = 0; i <= K + 1; i++) {
 				dp[row][col].cnt[i] = ((dp[row][col - 1].cnt[i] % MOD) + (dp[row - 1][col].cnt[i] % MOD)) % MOD;
+				dp[row][col].min[i] = min3(dp[row][col].min[i], dp[row - 1][col].min[i] + colCost[row][col], dp[row][col - 1].min[i] + rowCost[row][col]);
 
-				if (dp[row - 1][col].min[i] == 0 && dp[row][col - 1].min[i] == 0) // mark를 i번 지나서 오는 경우가 없음
-					continue;
-				else if (dp[row - 1][col].min[i] == 0) // mark를 i번 지나서 오는 경우는 왼쪽에서 오른쪽으로 오는 경우밖에 없음
-					addPathRight(row, col, i);
-				else if (dp[row][col - 1].min[i] == 0) // mark를 i번 지나서 오는 경우는 위에서 아래로 오는 경우밖에 없음
-					addPathDown(row, col, i);
-				else {
-					int fromUp = dp[row - 1][col].min[i] + colCost[row][col];
-					int fromLeft = dp[row][col - 1].min[i] + rowCost[row][col];
-					if (fromUp < fromLeft)
-						addPathDown(row, col, i);
-					else if (fromLeft < fromUp)
-						addPathRight(row, col, i);
-					else { // fromLeft == fromUp
-						char sel = direction(dp[row - 1][col].path[i], dp[row][col - 1].path[i]);
-						sel == 'd' ? addPathDown(row, col, i) : addPathRight(row, col, i);
-					}
-				}
+				if (dp[row - 1][col].min[i] + colCost[row][col] < dp[row][col - 1].min[i] + rowCost[row][col]) // 위에서 오는 경우
+					dp[row][col].path[i] = { row - 1,col };
+				else
+					dp[row][col].path[i] = { row,col - 1 };
 			}
 			if (mark[row][col]) {
 				dp[row][col].cnt[K + 1] = ((dp[row][col].cnt[K + 1] % MOD) + (dp[row][col].cnt[K] % MOD)) % MOD;
@@ -128,34 +97,45 @@ void solve() {
 					dp[row][col].path[i] = dp[row][col].path[i - 1];
 				}
 				dp[row][col].cnt[0] = 0;
-				dp[row][col].min[0] = 0;
-				dp[row][col].path[0] = "";
+				dp[row][col].min[0] = MAX;
 			}
 		}
 	}
 
 	// dp 출력 보기
-	//for (int i = 0; i < M; i++) {
-	//	for (int j = 0; j < N; j++) {
-	//		cout << "(";
-	//		for (int k = 0; k <= K + 1; k++) {
-	//			cout << dp[i][j].cnt[k] << ", " << dp[i][j].min[k] << ", " << dp[i][j].path[k] << "/";
-	//		}
-	//		cout << ") ";
-	//	}
-	//	cout << endl;
-	//}
-
-	auto& ans = dp[M - 1][N - 1];
-	for (int k = 0; k <= K; k++) {
-		if (ans.min[k] != 0) {
-			cout << "k:" << k << " count:" << ans.cnt[k] << " cost:" << ans.min[k] << endl;
-			cout << "(0,0)";
-			int x = 0, y = 0;
-			for (auto& s : ans.path[k]) {
-				s == 'R' ? ++y : ++x;
-				arrowPoint(x, y);
+	for (int i = 0; i < M; i++) {
+		for (int j = 0; j < N; j++) {
+			cout << "(";
+			for (int k = 0; k <= K + 1; k++) {
+				cout << dp[i][j].cnt[k] << "," << dp[i][j].min[k] << ",[" << dp[i][j].path[k].x << "," << dp[i][j].path[k].y << "]/";
 			}
+			//cout << dp[i][j].cnt[3] << "," << dp[i][j].min[3] << ",[" << dp[i][j].path[3].x << "," << dp[i][j].path[3].y << "]/";
+
+			cout << ") ";
+		}
+		cout << endl;
+	}
+
+	for (int k = 0; k <= K; k++) {
+		if (dp[M - 1][N - 1].cnt[k] != 0) { // 가는 경로가 있는 경우 출력
+			cout << "k:" << k << " count:" << dp[M - 1][N - 1].cnt[k] << " cost:" << dp[M - 1][N - 1].min[k] << endl;
+			vector<Point> vp;
+			int cx = M - 1, cy = N - 1, markCnt = k;
+			while (cx != 0 || cy != 0) {
+				vp.push_back({ cx,cy });
+				int nx = dp[cx][cy].path[markCnt].x, ny = dp[cx][cy].path[markCnt].y;
+				if (mark[cx][cy]) {
+					--markCnt;
+					//
+				}
+
+				cx = nx; cy = ny;
+				//표시된 곳에 왔을 때 점 이동. 점을 옮긴 뒤에 표시된 횟수를 줄여야 한다.
+			}
+			reverse(vp.begin(), vp.end());
+			cout << "(0,0)";
+			for (auto& a : vp)
+				cout << "->(" << a.x << "," << a.y << ")";
 			cout << endl;
 		}
 	}
